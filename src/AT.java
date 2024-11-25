6e404456bb030d55cb8db5554ad377f26ef1ee79
Alejandro Tineo Perez <alejandro.tineo@live.u-tad.com>
2024-11-25 20:23:07 +0100

    Modificación3

--- src/AT.java
+++ src/AT.java
@@ -3,7 +3,8 @@ public class AT {
 
 	public static void main(String[] args) {
 		System.out.println("Alumno:_Alejandro_Tineo");
-		System.out.println("Primera Modificación desde Master");
+		System.out.println("Modificación desde Master - Cambiado");
+
 
 	}
 
